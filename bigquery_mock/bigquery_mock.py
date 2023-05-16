999d2e9002a6ffc9921161169bfab24f6476ee3a
Henry <paulhtremblay@gmail.com>
2023-05-16 01:20:59 -0700

    methods for query plus tests

--- bigquery_mock/bigquery_mock.py
+++ bigquery_mock/bigquery_mock.py
@@ -1,3 +1,6 @@
+class InvalidData(Exception):
+    pass
+
 class SchemaField:
 
     def __init__(self):
@@ -28,11 +31,16 @@ class Row():
 
     def __init__(self, row):
         self.info = {}
+        l = []
+        for i in row:
+            self.info[i[0]] = i[1]
+            l.append(i[1])
+        self._values = tuple(l)
         self.row = row
 
     def get(self, *args, **kwargs):
         if args:
-            return self.info.get(args)
+            return self.info.get(args[0])
         return self.info.get(kwargs['key'])
 
     def items(self, *args, **kwargs):
@@ -40,29 +48,27 @@ class Row():
             yield i
 
     def values(self, *args, **kwargs):
-        return (),
+        return self._values
 
     def keys(self, *args, **kwargs):
-        return {}.keys()
-
-class Result():
-
-    def __init__(self, data):
-        assert False, 'not used'
-        self.data = data
-
-    def result(self):
-        return RowIterator(data = self.data)
-
-class Query:
-
-    def __init__(self, data):
-        self.data = data
+        return self.info.keys()
 
 class BigQueryMock:
 
-    def __init__(self, data = None):
+    def _test_valid_data(self):
+        if not isinstance(self.data, list):
+            raise InvalidData(f'{self.data} is not a list')
+        errors = []
+        for n, i in enumerate(self.data):
+            if not isinstance(i, list):
+                errors.append((n, i, 'not a list'))
+        if len(errors) != 0:
+            raise InvalidData(errors)
+
+
+    def __init__(self, data = []):
         self.data = data
+        self._test_valid_data()
 
     def query(self, *args, **kwargs):
         return RowIterator(data = self.data)
--- bigquery_mock/tests/test1.py
+++ bigquery_mock/tests/test1.py
@@ -1,8 +1,10 @@
 import sys
 sys.path.append('.')
-import unittest
-import bigquery_mock
 
+import unittest
+
+import bigquery_mock
+from bigquery_mock import InvalidData
 
 from google.cloud import bigquery
 DATA1= [
@@ -14,6 +16,17 @@ DATA1= [
         ('address', '1881 Congress Ave.'),
         ]
 ]
+
+def get_sql():
+        return """
+        SELECT
+      name, status, address
+    FROM
+      `bigquery-public-data.austin_bikeshare.bikeshare_stations`
+    LIMIT
+      2
+        """
+
 
 def items_func_with_result(bq_client, sql):
     row_iter = bq_client.query(sql).result()
@@ -33,42 +46,93 @@ def items_func_not_use_result(bq_client, sql):
         break
     return final
 
+def get_func_no_key(bq_client, sql):
+    row_iter = bq_client.query(sql)
+    final = []
+    for i in row_iter:
+        final.append(i.get('name'))
+    return final
+
+def get_func_with_key(bq_client, sql):
+    row_iter = bq_client.query(sql)
+    final = []
+    for i in row_iter:
+        final.append(i.get('name'))
+    return final
+
+def get_func_with_key_with_result(bq_client, sql):
+    row_iter = bq_client.query(sql).result()
+    final = []
+    for i in row_iter:
+        final.append(i.get('name'))
+    return final
+
+def values_func_with_key_with_result(bq_client, sql):
+    row_iter = bq_client.query(sql).result()
+    final = []
+    for i in row_iter:
+        final.append(i.values())
+    return final
+
+def keys_func_with_key_with_result(bq_client, sql):
+    row_iter = bq_client.query(sql).result()
+    final = []
+    for i in row_iter:
+        final.append(i.keys())
+    return final
 
 class TestResults(unittest.TestCase):
 
-    def test_items1(self):
-
-        sql = """
-        SELECT
-      name, status, address
-    FROM
-      `bigquery-public-data.austin_bikeshare.bikeshare_stations`
-    LIMIT
-      2
-        """
+    def test_items_first_result_returns_3_correct_name_values(self):
         client = bigquery_mock.BigQueryMock(data = DATA1)
-        #client = bigquery.Client()
-        f = items_func_with_result(bq_client = client, sql = sql)
+        f = items_func_with_result(bq_client = client, sql = get_sql())
         self.assertTrue(f[0], ('name', 'State Capitol @ 14th & Colorado'))
         self.assertTrue(f[1], ('status', 'closed'))
         self.assertTrue(f[2], ('address', '206 W. 14th St.'))
 
-    def test_items2(self):
-
-        sql = """
-        SELECT
-      name, status, address
-    FROM
-      `bigquery-public-data.austin_bikeshare.bikeshare_stations`
-    LIMIT
-      2
-        """
+    def test_items_first_result_not_using_result_method_returns_3_correct_name_values(self):
         client = bigquery_mock.BigQueryMock(data = DATA1)
-        #client = bigquery.Client()
-        f = items_func_not_use_result(bq_client = client, sql = sql)
+        f = items_func_not_use_result(bq_client = client, sql = get_sql())
         self.assertTrue(f[0], ('name', 'State Capitol @ 14th & Colorado'))
         self.assertTrue(f[1], ('status', 'closed'))
         self.assertTrue(f[2], ('address', '206 W. 14th St.'))
+
+    def test_items_no_values_returns_empty_list(self):
+        client = bigquery_mock.BigQueryMock()
+        f = items_func_with_result(bq_client = client, sql = get_sql())
+        self.assertEqual(f, [])
+
+    def test_get_name_returns_2_correct_names(self):
+        client = bigquery_mock.BigQueryMock(data = DATA1)
+        f = get_func_no_key(bq_client = client, sql = get_sql())
+        self.assertEqual(f, ['State Capitol @ 14th & Colorado', 'Bullock Museum @ Congress & MLK'])
+        
+    def test_get_name_with_key_word_arg_returns_2_correct_names(self):
+        client = bigquery_mock.BigQueryMock(data = DATA1)
+        f = get_func_with_key(bq_client = client, sql = get_sql())
+        self.assertEqual(f, ['State Capitol @ 14th & Colorado', 'Bullock Museum @ Congress & MLK'])
+
+    def test_get_name_with_key_word_arg_and_result_method_returns_2_correct_names(self):
+        client = bigquery_mock.BigQueryMock(data = DATA1)
+        f = get_func_with_key_with_result(bq_client = client, sql = get_sql())
+        self.assertEqual(f, ['State Capitol @ 14th & Colorado', 'Bullock Museum @ Congress & MLK'])
+
+    def test_values_returns_2_correct_values(self):
+        client = bigquery_mock.BigQueryMock(data = DATA1)
+        f = values_func_with_key_with_result(bq_client = client, sql = get_sql())
+        self.assertEqual(f[0], ('State Capitol @ 14th & Colorado', 'closed', '206 W. 14th St.'))
+
+    def test_keys_returns_correct_keys_first_result(self):
+        client = bigquery_mock.BigQueryMock(data = DATA1)
+        f = keys_func_with_key_with_result(bq_client = client, sql = get_sql())
+        self.assertTrue(list(f[0]), ['name', 'status', 'address'])
+
+    def test_not_a_list_data_raises_InvalidData(self):
+        self.assertRaises(InvalidData, bigquery_mock.BigQueryMock, data = 1)
+
+    def test_not_a_list_in_list_data_raises_InvalidData(self):
+        data = [[('name', 'value',),], 1] 
+        self.assertRaises(InvalidData, bigquery_mock.BigQueryMock, data = data)
         
 if __name__ == '__main__':
     unittest.main()
